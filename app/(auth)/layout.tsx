5caa439ce5b53f2ca10e66c08cf46bfdf1f3dcb3
Plode-v <plodevanichaka@gmail.com>
2023-08-26 12:10:06 +0700

    setup protected routes

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = ({children}: { children: React.ReactNode }) => {
+  return (
+    <div className='flex items-center justify-center h-screen bg-neutral-100'>
+        {children}
+    </div>
+  )
+}
+
+export default AuthLayout
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, { useEffect } from 'react'
 import Link from 'next/link'
+import { SignInButton, SignUpButton } from "@clerk/nextjs"
+
 
 const LandingPage = () => {
 
@@ -23,11 +25,18 @@ const LandingPage = () => {
       <div className='flex w-full lg:w-5/6 3xl:w-[1800px] flex-col items-center justify-center px-5'>
 
         {/* navbar section */}
-        <nav className='fixed top-0 border sm:my-5 my-2 w-[95%] lg:w-5/6 3xl:w-[1800px] flex justify-between sm:h-[60px] h-[50px] rounded-lg bg-blue-500 text-white items-center p-5 duration-[200ms]' id='navbar'>
+        <nav className='fixed top-0 sm:my-5 my-2 w-[95%] lg:w-5/6 3xl:w-[1800px] flex justify-between sm:h-[60px] h-[50px] rounded-lg bg-blue-500 text-white items-center sm:p-5 p-2 duration-[200ms]' id='navbar'>
           <div>
             <Link href="/">Logo</Link>
           </div>
-          <div>Right</div>
+          <div className='flex gap-2'>
+            <SignInButton afterSignInUrl='/dashboard' mode='modal'>
+              <p className='bg-white text-black sm:py-2 sm:px-3 p-1 text-[0.75rem] sm:text-[1rem] rounded-md cursor-pointer'>Sign In</p>
+            </SignInButton>
+              <SignUpButton afterSignInUrl='/dashboard' mode='modal'>
+                <p className='bg-white text-black sm:py-2 sm:px-3 p-1 text-[0.75rem] sm:text-[1rem] rounded-md cursor-pointer'>Sign Up</p>
+              </SignUpButton>
+          </div>
         </nav>
         <div className='h-[50px] sm:h-[60px] sm:my-5 my-2'>
 
