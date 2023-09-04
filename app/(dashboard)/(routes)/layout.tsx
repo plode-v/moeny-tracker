f2c7ab601e4009d58985dade6ac31c18e2103ca6
Plode-v <plodevanichaka@gmail.com>
2023-09-04 14:07:13 +0700

    Setup Firebase Auth

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,14 +1,15 @@
+'use client'
 import React from 'react'
 import Nav from '@/components/Nav'
 
 const DashboardLayout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div className='w-full flex justify-center h-screen bg-slate-900'>
-      <div className='w-full lg:w-3/4 3xl:w-1/2'>
-        <Nav />
-        {children}
+      <div className='w-full flex justify-center h-screen bg-slate-900'>
+        <div className='w-full lg:w-3/4 3xl:w-1/2'>
+          <Nav />
+          {children}
+        </div>
       </div>
-    </div>
   )
 }
 
